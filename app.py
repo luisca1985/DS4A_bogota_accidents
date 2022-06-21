f788718ecc71544661e612772394a5c341c8f099
Luis Camilo Jimenez Alvarez <luisca195@gmail.com>
2022-06-20 23:08:39 -0500

    callback translate

--- app.py
+++ app.py
@@ -1,33 +1,41 @@
-from os import path
+# Load your libraries
+import os
 from dash import Dash, callback, html, dcc, dash_table, Input, Output, State, MATCH, ALL
 import dash_bootstrap_components as dbc
 
-import plotly.express as px
+from dash.dependencies import Input, Output
 
+from deep_translator import GoogleTranslator
 
-app = Dash(__name__,
-           external_stylesheets=[dbc.themes.FLATLY],
+# Create the app
+
+app = Dash(__name__, external_stylesheets=[dbc.themes.FLATLY],
            meta_tags=[{'name': 'viewport', 'content': 'width=device-width, initial-scale=1.0'}])
-
-
-app.title = 'Bogotá - Accidents'
+app.title = 'Translator - Correlation One'
 
 # Layout
+app.layout = dbc.Container([
+    html.H1(["Google Translator"],
+            className="h-100 p-5 bg-light border rounded-3"),
+    dbc.Input(id="my-input", placeholder="Type something",
+              type="text", value="home"),
+    html.Br(),
+    html.P(id="my-output", children=['home'])
+])
 
-app.layout = dbc.Container(
-    [
-        dbc.Row("Titulo"),
-        dbc.Row([
-            dbc.Col("Menú Lateral", md=3),
-            dbc.Col("Contenido", md=9)
-        ]),
-        dbc.Row("Pie de Página")
+# Callback
 
-    ]
+
+@app.callback(
+    Output(component_id='my-output', component_property='children'),
+    Input(component_id='my-input', component_property='value')
 )
+def update_output_div(input_value):
+    translated_input_text = GoogleTranslator(
+        source='auto', target='es').translate(input_value)
+    return f'Your translation:  "{translated_input_text}"'
 
-# Callbacks
 
 # Start the server
 if __name__ == '__main__':
-    app.run_server(host='0.0.0.0', port='8050', debug=True)
+    app.run_server(host="0.0.0.0", port="8050", debug=True)

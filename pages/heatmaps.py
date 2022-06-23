0efe45a15ce2c12a04fb1968f00e691b2e622af8
Luis Camilo Jimenez Alvarez <luisca1985@gmail.com>
2022-06-22 22:05:59 -0500

    format heatmap

--- pages/heatmaps.py
+++ pages/heatmaps.py
@@ -1,13 +1,12 @@
+from data.clean import get_data_cleaned
+import plotly.express as px
+from dash import Dash, dcc, html, Input, Output, callback
 import dash
 from dash_labs.plugins import register_page
 import pandas as pd
 
 register_page(__name__, path="/heatmaps")
 
-from dash import Dash, dcc, html, Input, Output, callback
-import plotly.express as px
-
-from data.clean import get_data_cleaned
 
 # https://plotly.com/python/px-arguments/
 # df = px.data.medals_wide(indexed=True)
@@ -17,34 +16,35 @@ layout = html.Div(
     [
         html.P("Medals included:"),
         dcc.Dropdown(
-                    id="heatmaps-medals",
-                    options=[
-                        {"label": "CHAPINERO", "value": "CHAPINERO"},
-                        {"label": "USAQUEN", "value": "USAQUEN"},
-                        {"label": "FONTIBON", "value": "FONTIBON"},
-                    ],
-                    # value=['CHAPINERO', 'USAQUEN', 'FONTIBON'],
-                    multi = False
-                ),
+            id="heatmaps-medals",
+            options=[
+                {"label": "CHAPINERO", "value": "CHAPINERO"},
+                {"label": "USAQUEN", "value": "USAQUEN"},
+                {"label": "FONTIBON", "value": "FONTIBON"},
+            ],
+            # value=['CHAPINERO', 'USAQUEN', 'FONTIBON'],
+            multi=False
+        ),
         dcc.Graph(id="heatmaps-graph"),
     ], className='card'
 )
 
+
 @callback(
-    Output("heatmaps-graph", "figure"), 
+    Output("heatmaps-graph", "figure"),
     Input("heatmaps-medals", "value"))
 def filter_heatmap(borough):
-    df2=df[df['borough']==borough]
-    seg = df2.groupby(['year','month']).size().to_frame('number_of_accident').reset_index()
+    df2 = df[df['borough'] == borough]
+    seg = df2.groupby(['year', 'month']).size().to_frame(
+        'number_of_accident').reset_index()
     seg['year'] = seg['year'].astype('category')
     fig = px.bar(seg, x='month', y='number_of_accident', color='year', text_auto=True, labels={
                         'month': '',
                         'number_of_accident': 'NUMBER OF ACCIDENTS',
                         'year': 'Select one/multiple years'
-                    },
-                    title='ACCIDENTS PER YEAR')
+    },
+        title='ACCIDENTS PER YEAR')
     # fig.show()
-
 
     # https://plotly.com/python/imshow/
     # fig = px.imshow(df.head()[cols])

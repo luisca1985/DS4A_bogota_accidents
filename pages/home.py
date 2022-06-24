15dda42f602e3f7b1f663d319eeb709693ce6e3c
Luis Camilo Jimenez Alvarez <luisca1985@gmail.com>
2022-06-23 21:09:34 -0500

    front-end

--- pages/home.py
+++ pages/home.py
@@ -17,26 +17,67 @@ kpi4 = kpibadge('FRIDAY', 'Day with more accidents', 'Danger')
 
 mapa_ejemplo = mapsample('Mapa de ejemplo', 'id_mapa_ejemplo')
 
-layout=  dbc.Container(
+# layout=  dbc.Container(
+#     [
+#         dbc.Row([
+#             dbc.Col([
+#                 kpi1.display()
+#             ], className='card'),
+#             dbc.Col([
+#                 kpi2.display()
+#             ], className='card'),
+#             dbc.Col([
+#                 kpi3.display()
+#             ], className='card'),
+#             dbc.Col([
+#                 kpi4.display()
+#             ], className='card')
+#         ]),
+#         dbc.Row([
+#             dbc.Col([
+#                 mapa_ejemplo.display()
+#             ], xs=12, className='card'),            
+#         ]),     
+#     ]
+# ) 
+
+# SIDEBAR_STYLE = {
+#     # "position": "fixed",
+#     "top": 0,
+#     "left": 0,
+#     "bottom": 0,
+#     "width": "16rem",
+#     "padding": "2rem 1rem",
+#     "background-color": "#f8f9fa",
+# }
+
+check_year = dcc.Checklist(
+    sorted(list(range(2015,2022)),reverse=True),
+    [2021],
+    inline=False
+)
+
+sidebar = html.Div(
     [
-        dbc.Row([
-            dbc.Col([
-                kpi1.display()
-            ], className='card'),
-            dbc.Col([
-                kpi2.display()
-            ], className='card'),
-            dbc.Col([
-                kpi3.display()
-            ], className='card'),
-            dbc.Col([
-                kpi4.display()
-            ], className='card')
-        ]),
-        dbc.Row([
-            dbc.Col([
-                mapa_ejemplo.display()
-            ], xs=12, className='card'),            
-        ]),     
+        html.H4("Filters"),
+        html.Hr(),
+        html.H5(
+            "Year"
+        ),
+        check_year,
+    ],
+    # style=SIDEBAR_STYLE,
+)
+
+
+layout = dbc.Container(
+    [
+        dbc.Row(
+            [
+                dbc.Col(sidebar, md=3),
+                dbc.Col('Contenido', md=9)
+            ]
+        )
     ]
-) 
+
+)

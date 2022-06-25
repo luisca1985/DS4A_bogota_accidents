54dde23c5e811fdadb7e032805f70117ec55998d
Alejandro Moreno Fresneda <magnandro@hotmail.com>
2022-06-25 17:10:16 -0500

    Front-end work

--- app.py
+++ app.py
@@ -2,36 +2,53 @@
 import dash
 import dash_labs as dl
 import dash_bootstrap_components as dbc
+from dash import html
 import os
 #from callbacks import register_callbacks
 
 # Dash instance declaration
 app = dash.Dash(__name__, plugins=[dl.plugins.pages], external_stylesheets=[dbc.themes.FLATLY],)
 
-
+PLOTLY_LOGO = "https://hopin.com/quiin/organizations/pictures/000/098/495/original/ds4a-logo_2x.png"
 
 #Top menu, items get from all pages registered with plugin.pages
-navbar = dbc.NavbarSimple(
-    [
-
-    dbc.NavItem(dbc.NavLink( "Inicio", href='/')),
-    dbc.DropdownMenu(
+navbar = dbc.Navbar(
+    dbc.Container(
         [
-            
-            dbc.DropdownMenuItem(page["name"], href=page["path"])
-            for page in dash.page_registry.values()
-            if page["module"] != "pages.not_found_404"
-        ],
-        nav=True,
-        label="Data Science",
+            html.A(
+                # Use row and col to control vertical alignment of logo / brand
+                dbc.Row(
+                    [
+                        dbc.Col(html.Img(src=PLOTLY_LOGO, height="30px")),
+                        dbc.Col(dbc.NavbarBrand("Analyzing Traffic Accidents in Bogota during 2015 to 2021", className="ms-8")),
+                    ],
+                    align="center",
+                    className="g-4",
+                ),
+                href="https://plotly.com",
+                style={"textDecoration": "none", "color":"#424242"},
+            ),
+            dbc.NavItem(dbc.NavLink( "Inicio", href='/'),className="g-0 ms-auto flex-nowrap mt-3 mt-md-0"),
+            dbc.NavItem(dbc.NavLink("Nosotros", href="/nosotros")),
+            dbc.DropdownMenu(
+                [
+                    
+                    dbc.DropdownMenuItem(page["name"], href=page["path"])
+                    for page in dash.page_registry.values()
+                    if page["module"] != "pages.not_found_404"
+                ],
+                nav=True,
+                label="Data Science",
+            ),
+        ]
     ),
-    dbc.NavItem(dbc.NavLink("Nosotros", href="/nosotros")),
-    ],
-    brand="Analyzing Traffic Accidents in Bogota during 2015 to 2021",
     color="#FAC63F",
-    dark=True,
+    dark=False,
     className="mb-2",
 )
+
+
+
 
 #Main layout
 app.layout = dbc.Container(
@@ -43,6 +60,9 @@ app.layout = dbc.Container(
     fluid=True,
 )
 
+
+
+
 # Call to external function to register all callbacks
 #register_callbacks(app)
 
@@ -53,3 +73,5 @@ server = app.server
 # Testing server, don't use in production, host
 if __name__ == "__main__":
     app.run_server(host='0.0.0.0', port=8050, debug=True)
+
+
